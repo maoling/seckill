2b25535588ebc121e5a4121e4964670a9b9f9420
maoling <maoling199210191@sina.com>
2016-08-15 01:03:28 +0800

    计时交互

--- src/main/webapp/resources/script/seckill.js
+++ src/main/webapp/resources/script/seckill.js
@@ -1,7 +1,10 @@
-//�����Ҫ�����߼�js����
-// javascript ģ�黯
 var seckill = {
 	URL : {
+		now : function(){
+			return '/seckill/time/now';
+		}
+	},
+	handleSeckillkill : function(){
 		
 	},
 	validatePhone : function(phone){
@@ -11,12 +14,28 @@ var seckill = {
 			return false;
 		}
 	},
+	countdown : function(seckillId,nowTime,startTime,endTime){
+		var seckillBox = $('#seckill-box');
+		if(nowTime > endTime){
+			seckillBox.html('秒杀结束！');
+		}else if(nowTime <　startTime){
+			var killTime = new Date(startTime +　1000);
+			seckillBox.countdown(killTime,function(event){
+				var format = event.strftime('秒杀倒计时: %D天  %H时  %M分 %S秒');
+				seckillBox.html(format);
+				/*时间完成后回调事件*/
+			}).on('finish.countdown',function(){
+				seckill.handleSeckillkill();
+			});
+		}else{
+			//执行秒杀逻辑
+			seckill.handleSeckillkill();
+		}
+	},
 	detail : {
 		init : function(params){
 			var killPhone = $.cookie('killPhone');
-			var startTime = params['startTime'];
-			var endTime = params['endTime'];
-			var seckillId = params['seckillId'];
+			
 			if(!seckill.validatePhone(killPhone)){
 			    var killPhoneModal = $("#killPhoneModal");
 			    killPhoneModal.modal({
@@ -31,12 +50,22 @@ var seckill = {
 			    		$.cookie('killPhone',inputPhone,{expires:7,path:'/seckill'});
 			    		window.location.reload();
 			    	}else{
-			    		$('#killPhoneMessage').hide().html('<lable class="label label-danger">�ֻ�Ŵ���</lable>').show(500);
+			    		$('#killPhoneMessage').hide().html('<lable class="label label-danger">手机号码错误！</lable>').show(500);
 			    	}
 			    });
 			}
+			var startTime = params['startTime'];
+			var endTime = params['endTime'];
+			var seckillId = params['seckillId'];
 			//已经登录，计时交互
-			$.get('/seckill/time/')
+			$.get(seckill.URL.now(),{},function(result){
+				if(result && result['success']){
+					var nowTime = result['data'];
+					seckill.countdown(seckillId,nowTime,startTime,endTime);
+				}else{
+					console.log('result:'+result);
+				}
+			});
 			
 			
 		}
